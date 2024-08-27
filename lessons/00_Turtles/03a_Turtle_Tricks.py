5fdba1bee82ba3d198c42036182e90baf0590dcd
Eric Busboom <eric@civicknowledge.com>
2024-08-27 20:56:02 +0000

    Getting started with Tina!

--- lessons/00_Turtles/03a_Turtle_Tricks.py
+++ lessons/00_Turtles/03a_Turtle_Tricks.py
@@ -10,10 +10,13 @@ to see how to use the turtle commands.
 
 # These lines are needed in most turtle programs
 import turtle                           # Tell Python we want to work with the turtle
-turtle.setup (width=600, height=600)    # Set the size of the window
+turtle.setup (width=500, height=500)    # Set the size of the window
 
 tina = turtle.Turtle()                  # Create a turtle named tina
-
+tina.forward(150)
+tina.right(120)
+tina.forward(150)
+tina.right(120)
 # Use tina.forward() and tina.left() to draw a triangle
 # Make each side of the triangle a different color with 
 # tina.pencolor()
